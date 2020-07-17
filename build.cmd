70678c35c49eb3edb2b2254d723f59eb43644cc4
Mark Szolkowski <szeliga@hotmail.com>
2020-07-17 21:36:52 +0100

    Added a comment at the beginning about checking the path to "pasmo.exe"

--- build.cmd
+++ build.cmd
@@ -1,1 +1,2 @@
+REM Check the path to your pasmo.exe  -  I don't want it on my path 'cos there's enough there already!
 "d:\Users\Mark\GoogleDrive\Retro Stuff\ZX Spectrum\06 - Development\Assemblers\pasmo-0.5.3\pasmo.exe" -v --tapbas MontyHall.asm MontyHall.tap
